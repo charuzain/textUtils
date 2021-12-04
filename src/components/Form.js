79cc7e2a88f4695271979c069b241b733dc36bac
charuzain <charuabhizain@gmail.com>
2021-12-04 10:39:15 -0500

    Lowercase button and functionality added

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,23 +4,25 @@ export default function Form(props) {
   const convertToUp =()=>{
     const newText = text.toUpperCase();
     setText(newText)
-    console.log("button was clicked")
 
   }
+  const convertToLower =()=>{
+   const newText = text.toLowerCase();
+   setText(newText)
+  }
+
   const handleOnChange = (event)=>{
     setText(event.target.value)
-    console.log("on Change")
   }
-  const [text, setText] = useState("Enter your text here");
+  const [text, setText] = useState("");
   return (
   <div>
     <h1>{props.heading}</h1>   
       <div className ="mb-3">
             <textarea className ="form-control" id="myBox" value = {text} onChange={handleOnChange} rows="8 "></textarea>
             </div>
-            <button className="btn btn-primary" onClick={convertToUp}>Convert to uppercase</button>
-   
-     
+            <button className="btn btn-primary" onClick={convertToUp}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-3" onClick={convertToLower}>Convert to Lowercase</button>  
       </div>
   )
 }
