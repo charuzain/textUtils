e6797482c7a71e7e723252935e91b59344781fea
charuzain <charuabhizain@gmail.com>
2021-12-04 00:16:35 -0500

    useState Hook added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Form(props) {
+  const convertToUp =()=>{
+    const newText = text.toUpperCase();
+    setText(newText)
+    console.log("button was clicked")
+
+  }
+  const handleOnChange = (event)=>{
+    setText(event.target.value)
+    console.log("on Change")
+  }
+  const [text, setText] = useState("Enter your text here");
   return (
   <div>
     <h1>{props.heading}</h1>   
       <div className ="mb-3">
-            <textarea className ="form-control" id="myBox" rows="8 "></textarea>
+            <textarea className ="form-control" id="myBox" value = {text} onChange={handleOnChange} rows="8 "></textarea>
             </div>
-            <button className="btn btn-primary">Convert to uppercase</button>
+            <button className="btn btn-primary" onClick={convertToUp}>Convert to uppercase</button>
    
      
       </div>
