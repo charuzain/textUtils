f0fa8660e7bed60112150222f9145baec9d9b952
charuzain <charuabhizain@gmail.com>
2021-12-19 22:28:04 -0500

    Alert compoenent added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
+import './App.css';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null)
+
+  const showAlert =(type , message)=>{
+    setAlert({
+      type : type,
+      message :message
+    })
+    setTimeout(() => { setAlert("") }, 1000)
+
+  }
+
+  const handleToggle = function(){
+    if(mode === 'dark'){
+    setMode('light')
+    document.body.style.backgroundColor ="white"
+      showAlert("success" , "Light mode has been enabled")
+  }
+    else{
+      setMode('dark')
+      document.body.style.backgroundColor = "grey"
+      showAlert("success", "Dark mode has been enabled")
+    }
+
+    }
+
+
   return (
     <>
-    <Navbar title = "TextUtil"/>
+    <Navbar title = "TextUtil" mode={mode} handleToggle={handleToggle}/>
+      <Alert alert={alert}/>
     <div className="container my-3">
-     <Form heading="Enter the text"/>
+     <Form heading="Enter the text" showAlert={showAlert}/>
    </div>
     </>
   );
