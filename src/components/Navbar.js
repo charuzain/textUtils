94223ca8b077cbe475d0733894cf6f8485e2ede0
charuzain <charuabhizain@gmail.com>
2021-12-20 16:22:10 -0500

    Routing functionality implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 // rfc : shortcut for react function based component
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 // impt : shortcut for importing prop-type
 export default function Navbar(props){
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">{props.title}</a>
+      <Link className="navbar-brand" to="/">{props.title}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">{props.nav1}</a>
+            <Link className="nav-link active" aria-current="page" to="/">{props.nav1}</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">{props.nav2}</a>
+            <Link className="nav-link" to="/about">{props.nav2}</Link>
           </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">{props.nav3}</Link>
+            </li>
+
         </ul>
         {/* <form classNameName="d-flex">
           <input classNameName="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
@@ -45,6 +50,6 @@ Navbar.propTypes = {
  Navbar.defaultProps ={
  nav1: "Home",
  nav2: "About",
-   nav3 : "search"}
+   nav3 : "Contact Us"}
 
 
