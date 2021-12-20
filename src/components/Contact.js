0bff9d9d9d9eb360e5f30a7b7e04e40ec0e10f23
charuzain <charuabhizain@gmail.com>
2021-12-20 16:19:19 -0500

    Contact component implemented

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us : abc@gmail.com</h1>
+        <h3>Call Us : 123456</h3>
+    </div>
+  )
+}
