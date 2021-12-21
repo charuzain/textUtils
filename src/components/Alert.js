bcb7b8a62abd42b50e64af028d835d8c22d31cb9
charuzain <charuabhizain@gmail.com>
2021-12-21 17:44:57 -0500

    Fixed bugs

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,10 +6,11 @@ export default function Alert(props) {
       const upper = word.toLowerCase();
       return upper.charAt(0).toUpperCase()+upper.slice(1)}
   return (
-    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <div style={{height:'50px'}}>
+   { props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
       <strong>{capitalize(props.alert.type)}</strong> : {props.alert.message}
       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>}
     </div>
-    
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -39,17 +39,17 @@ export default function Form(props) {
       <div className ="mb-3">
             <textarea className ="form-control" id="myBox" value = {text} onChange={handleOnChange} rows="8 "></textarea>
             </div>
-            <button className="btn btn-primary" onClick={convertToUp}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={convertToLower}>Convert to Lowercase</button> 
-            <button className="btn btn-primary mx" onClick={clearText}>Clear</button>  
-           <button className="btn btn-primary mx-2" onClick={copyText}>Copy</button>  
+            <button disabled ={text.length===0} className="btn btn-primary" onClick={convertToUp}>Convert to Uppercase</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={convertToLower}>Convert to Lowercase</button> 
+        <button disabled={text.length === 0} className="btn btn-primary mx my-1" onClick={clearText}>Clear</button>  
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={copyText}>Copy</button>  
 
 
       </div>
       <div className="container my-4">
         <h4>Text summary</h4>
-        <p>Words:{text.split(" ").length-1} Characters:{text.length}</p>
-        <p>Time to read : {0.008*text.split(" ").length}</p>
+        <p>Words:{text.split(" ").filter(elem => elem.length !== 0).length} Characters:{text.length}</p>
+        <p>Time to read : {0.008 * text.split(" ").filter(elem => elem.length !== 0).length}</p>
         <h3>Preview</h3>
         {text}
         </div> 
